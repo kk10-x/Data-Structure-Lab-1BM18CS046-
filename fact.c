f5b57d93db348ef790210d7629d295c0f95233e9
kk10-x <53857387+kk10-x@users.noreply.github.com>
2019-08-27 21:53:35 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int fact(int n)
+{
+  if(n==0)
+  return 1;
+  else
+  return (n*fact(n-1));
+}
+void main()
+{
+int s,n;
+printf("Enter the number till which the factroial is required\n");
+scanf("%d",&n);
+s=fact(n);
+printf("the factroial is: %d",s);
+}
+
